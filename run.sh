180aedad69e57cb0a0d405c021f1ade39cb83f72
Lefevre Donovan <lfv.donovan@gmail.com>
2023-12-12 14:24:24 +0100

    IHM

--- run.sh
+++ run.sh
@@ -8,6 +8,6 @@ cd bin
 
 FX_PATH="../lib/javafx-sdk-17.0.9/lib/"
 
-java --module-path "$FX_PATH" --add-modules javafx.controls,javafx.fxml App
+java --module-path "$FX_PATH" --add-modules javafx.controls,javafx.fxml vue.App
 
 read -p "Tapez enter pour continuer..."
